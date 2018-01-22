1b481f171bcae0e1311302b5f60e219559f93ba9
Morgan Fudge <mfudge92@gmail.com>
2018-01-22 00:39:36 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -11,4 +11,10 @@ class App < Sinatra::Base
 
     erb :reversed
   end
+
+  get '/friends' do
+  @friends = ['Emily Wilding Davison', 'Harriet Tubman', 'Joan of Arc', 'Malala Yousafzai', 'Sojourner Truth']
+ 
+  erb :friends
+end
 end
